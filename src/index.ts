2f8904b0b1f0b5afb925b7eecbdbf8a86f0184f2
pyros2097 <pyros2097@gmail.com>
2021-04-13 21:39:02 +0530

    improve package

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import { Table } from './dynamodb';
-import { Lambda } from './lambda';
-import { Bucket } from './s3';
-import { getToken } from './jwt';
-
-export { Table, Bucket, Lambda, getToken };
